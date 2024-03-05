cd743b5125daaf5b7989e9547b548eab5c0184d1
jessicaduarte95 <jessicaduarte.95@gmail.com>
2024-03-05 14:20:11 -0300

    cadastrar pessoas

--- src/app/modules/pessoas/pessoas.module.ts
+++ src/app/modules/pessoas/pessoas.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { PessoasComponent } from './pages/pessoas/pessoas.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    PessoasComponent
+    PessoasComponent,
+    CadastroComponent
   ],
   imports: [
     CommonModule
